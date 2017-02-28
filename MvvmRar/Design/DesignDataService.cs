9e7b0796812a5287342b0b3d0ad6fd071dcde19c
adm1 <adm1@FIN>
2017-02-28 15:18:08 +0300

    Добавьте файлы проекта.

---
+++ MvvmRar/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MvvmRar.Model;
+
+namespace MvvmRar.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome __ to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
---
+++ MvvmRar/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MvvmRar.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
