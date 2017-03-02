db941c2afeb7ff01524e5a43c36fd5e5229a5c68
Сергей Черкасских <6223726@mail.ru>
2017-03-02 23:24:37 +0300

    1

--- MvvmRarTests/Model/DataServiceTests.cs
+++ MvvmRarTests/Model/DataServiceTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MvvmRar.Rar;
+using Moq;
 
 namespace MvvmRar.Model.Tests
 {
@@ -18,15 +20,56 @@ namespace MvvmRar.Model.Tests
             DataService ds = new DataService();
 
             // act
+            var result = ds.RarFormF6Data;
             //Assert
 
-            Assert.IsNotNull(ds.RarFormF6Data);
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
         public void GetDataTest()
         {
-            Assert.IsTrue(true);
+            // arrange
+            RarFormF6 result1 = new RarFormF6();
+            Exception result2=new Exception();
+            DataService ds = new DataService();
+            //act
+            ds.GetData(
+                  (data, error) =>
+                  {
+                      result1 = data;
+                      result2 = error;
+                  });
+
+            //Assert
+            Assert.AreSame(ds.RarFormF6Data, result1, "RarFormF6");
+            Assert.IsNull(result2,"Exception");
         }
+
+        //[TestMethod()]
+        //public void GetDataTest1()
+        //{
+
+
+
+
+        //    Mock<IDataService> mockDataService = new Mock<IDataService>();
+        //    mockDataService.Setup(m => m.GetData(It.IsAny<Action<RarFormF6, Exception>>()));
+        //    //mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+
+        //    RarFormF6 result1 = new RarFormF6();
+        //    Exception result2 = new Exception();
+        //    //act
+        //    mockDataService.Object.GetData(
+        //          (data, error) =>
+        //          {
+        //              result1 = data;
+        //              result2 = error;
+        //          });
+            
+        //    //Assert
+        //    //Assert.AreSame(mockDataService.RarFormF6Data, result1, "RarFormF6");
+        //    //Assert.IsNull(result2, "Exception");
+        //}
     }
 }
