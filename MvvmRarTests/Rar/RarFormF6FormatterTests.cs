17f2b6689e126c57c98e4bd1c7c5e1fd60d5d75e
Сергей Черкасских <6223726@mail.ru>
2017-03-08 15:59:17 +0300

    7

--- MvvmRarTests/Rar/RarFormF6FormatterTests.cs
+++ MvvmRarTests/Rar/RarFormF6FormatterTests.cs
@@ -39,6 +39,27 @@ namespace MvvmRar.Rar.Tests
         [TestMethod()]
         public void RarFormF6FormatterTest()
         {
+            RarFormF6Formatter f6formatter = new RarFormF6Formatter();
+            var privateObject = new PrivateObject(f6formatter);
+            //privateObject.SetField("name",_name);
+            //privateObject.SetProperty("Name"? name);
+
+            XElement el = new XElement("root", 
+                new XAttribute("НаимПрог", "1СProgram"),
+                new XAttribute("ВерсФорм", "3.1"),
+                new XAttribute("ДатаДок", "01.01.2015"),
+                    new XElement("ФормаОтч",
+                        new XAttribute("НомФорм", "6.1"),
+                        new XAttribute("ПризПериодОтч", "0"),
+                        new XAttribute("ГодПериодОтч", "2015")
+                    
+                    )
+
+                
+                );
+            RarFormF6 formF6 = new RarFormF6();
+            privateObject.Invoke("SetupHeader", el, formF6);
+
             Assert.Fail();
         }
 
