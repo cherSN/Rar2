7a2959d7ebf5beeb48821c830f32753a270e846e
Сергей Черкасских <6223726@mail.ru>
2017-03-28 23:16:45 +0300

    Adress properties

--- MvvmRar/ViewModel/RarCompanyViewModelWrapper.cs
+++ MvvmRar/ViewModel/RarCompanyViewModelWrapper.cs
@@ -16,17 +16,129 @@ namespace MvvmRar.ViewModel
 
         public RarCompanyViewModelWrapper(RarCompany company) // : base(company)
         {
-            Company = company;
+            _Company = company;
         }
-
-        public string Name { get => Company.Name; set  { Company.Name = value; RaisePropertyChanged("Name"); } }
+        public RarCompany Company { get => _Company; set => _Company = value; }
+        public string Name { get => Company.Name; set { Company.Name = value; RaisePropertyChanged("Name"); } }
         public string INN { get => Company.INN; set { Company.INN = value; RaisePropertyChanged("INN"); } }
         public string KPP { get => Company.KPP; set { Company.KPP = value; RaisePropertyChanged("KPP"); } }
 
-        public RarCompany Company { get => _Company; set => _Company = value; }
+        #region -Adress Properties -
+        public string CountryId
+        {
+            get => (Company.Adress != null ? Company.Adress.CountryId : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.CountryId = value;
+                RaisePropertyChanged("CountryId");
+            }
+        }
+        public string PostCode
+        {
+            get => (Company.Adress != null ? Company.Adress.PostCode : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.PostCode = value;
+                RaisePropertyChanged("PostCode");
+            }
+        }
+        public string RegionId
+        {
+            get => (Company.Adress != null ? Company.Adress.RegionId : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.RegionId = value;
+                RaisePropertyChanged("RegionId");
+            }
+        }
+        public string District
+        {
+            get => (Company.Adress != null ? Company.Adress.District : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.District = value;
+                RaisePropertyChanged("District");
+            }
+        }
+        public string City
+        {
+            get => (Company.Adress != null ? Company.Adress.City : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.City = value;
+                RaisePropertyChanged("City");
+            }
+        }
+        public string Locality
+        {
+            get => (Company.Adress != null ? Company.Adress.Locality : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.Locality = value;
+                RaisePropertyChanged("Locality");
+            }
+        }
+        public string Street
+        {
+            get => (Company.Adress != null ? Company.Adress.Street : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.Street = value;
+                RaisePropertyChanged("Street");
+            }
+        }
+        public string Building
+        {
+            get => (Company.Adress != null ? Company.Adress.Building : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.Building = value;
+                RaisePropertyChanged("Building");
+            }
+        }
+        public string Block
+        {
+            get => (Company.Adress != null ? Company.Adress.Block : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.Block = value;
+                RaisePropertyChanged("Block");
+            }
+        }
+        public string Litera
+        {
+            get => (Company.Adress != null ? Company.Adress.Litera : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.Litera = value;
+                RaisePropertyChanged("Litera");
+            }
+        }
+        public string Apartment
+        {
+            get => (Company.Adress != null ? Company.Adress.Apartment : null);
+            set
+            {
+                if (Company.Adress == null) Company.Adress = new RarAdress();
+                Company.Adress.Apartment = value;
+                RaisePropertyChanged("Apartment");
+            }
+        } 
+        #endregion
 
-        public override string ToString() {
-            return Name+"; INN:"+INN+"; KPP:"+KPP;
+        public override string ToString()
+        {
+            return Name + "; INN:" + INN + "; KPP:" + KPP;
         }
 
     }
